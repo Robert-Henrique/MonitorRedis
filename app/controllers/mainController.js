0e36d1b7228f5113270ceca357e5f9eeea50a2f7
Robert Henrique <roberthenrique.ucdb@hotmail.com>
2018-01-25 14:14:19 -0300

    Deletando arquivos alocados em pastas erroneamente

--- app/controllers/mainController.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('app')
-.constant('SAVE_SETTING', true)
-    .controller('MainController', ['$scope', '$rootScope', '$http', '$timeout', '$location', '$interval', function ($scope, $rootScope, $http, $timeout, $location, $interval) {
-        //some general variables
-        $scope.ace = $scope.ace || {};
-        $scope.ace.settings = $scope.ace.settings || {};
-
-    }]);
--- app/services/homeService.js
+++
@@ -1,6 +0,0 @@
-﻿app.service("homeService", function ($http) {
-
-    this.obter = function () {
-        return $http.get("api/Home/Get");
-    };
-});
