d99d0c0f873725f5c60056211bc41fdfd3fd7818
Robert Henrique <roberthenrique.ucdb@hotmail.com>
2018-01-29 15:30:51 -0300

    Adicionando a classe gritter-top-right

--- MonitorRedis/app/modules/gritterModule.js
+++ MonitorRedis/app/modules/gritterModule.js
@@ -1,13 +1,13 @@
 ﻿angular.module("app").factory('$gritter', function () {
     return {
         success: function (msg, title) {
-            return jQuery.gritter.add({ title: title || "Sucesso", text: msg, class_name: 'gritter-success', time: 3000 });
+            return jQuery.gritter.add({ title: title || "Sucesso", text: msg, class_name: 'gritter-success gritter-top-right', time: 3000 });
         },
         error: function (msg, title) {
-            return jQuery.gritter.add({ title: title || "Erro", text: msg, class_name: 'gritter-error', time: 3000 });
+            return jQuery.gritter.add({ title: title || "Erro", text: msg, class_name: 'gritter-error gritter-top-right', time: 3000 });
         },
         warning: function (msg, title) {
-            return jQuery.gritter.add({ title: title || "Atenção", text: msg, class_name: 'gritter-warning', time: 3000 });
+            return jQuery.gritter.add({ title: title || "Atenção", text: msg, class_name: 'gritter-warning gritter-top-right', time: 3000 });
         }
     }
 });
